61f8e3b047f79d46e7422c2a83f336901f2d30c5
Scott Friedman <3011922+scttfrdmn@users.noreply.github.com>
2025-11-08 17:40:12 -0800

    📦 RELEASE: Update version to v0.5.12

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -16,7 +16,7 @@ import (
 var (
 	// Version is the current version of Prism.
 	// Should be in the format MAJOR.MINOR.PATCH.
-	Version = "0.5.10-dev"
+	Version = "0.5.12"
 
 	// GitCommit is the git commit hash of the build.
 	GitCommit = ""
