09d268b6277bb795fb07eeaf18ebfd7c89316883
Scott Friedman <3011922+scttfrdmn@users.noreply.github.com>
2026-01-24 00:08:24 -0800

    chore: Bump version to 0.7.2

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -16,7 +16,7 @@ import (
 var (
 	// Version is the current version of Prism.
 	// Should be in the format MAJOR.MINOR.PATCH.
-	Version = "0.7.1"
+	Version = "0.7.2"
 
 	// GitCommit is the git commit hash of the build.
 	GitCommit = ""
