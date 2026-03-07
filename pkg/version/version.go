8e9d0e9143c1a386e6c5a117ad4ebd7e7a66c688
Scott Friedman <3011922+scttfrdmn@users.noreply.github.com>
2026-03-06 16:58:41 -0800

    chore: bump version to v0.8.5

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -16,7 +16,7 @@ import (
 var (
 	// Version is the current version of Prism.
 	// Should be in the format MAJOR.MINOR.PATCH.
-	Version = "0.8.4"
+	Version = "0.8.5"
 
 	// GitCommit is the git commit hash of the build.
 	GitCommit = ""
